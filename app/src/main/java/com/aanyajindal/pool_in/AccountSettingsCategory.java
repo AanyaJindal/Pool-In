114388e3f3c153710b6690773c61d932e5c28a8f
jatin-jt <sachinjatingupta@gmail.com>
2016-09-03 17:56:59 +0530

    Split input skills in an array and

--- app/src/main/java/com/aanyajindal/pool_in/AccountSettingsCategory.java
+++ app/src/main/java/com/aanyajindal/pool_in/AccountSettingsCategory.java
@@ -36,13 +36,21 @@ public class AccountSettingsCategory extends AppCompatActivity {
 
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+
+
         mainDatabase = FirebaseDatabase.getInstance().getReference();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String string = etSkillSet.getText().toString();
+                String[] parts = string.split(",");
+                for(int i= 0;i<parts.length;i++){
+                    usersList.child(user.getUid()).child("skills").push().setValue(parts[i]);
+                }
                 usersList  = mainDatabase.child("users");
                 usersList.child(user.getUid()).setValue(new User(user.getDisplayName(), user.getEmail(),etCollege.getText().toString(),etlocation.getText().toString()));
-                usersList.child(user.getUid()).child("skills").push().setValue(etSkillSet.getText().toString());
+
             }
         });
 
