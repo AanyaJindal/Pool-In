96d57212f14ed762b331d4a231b2dbb431b25306
aanyajindal <aanyajindal05@gmail.com>
2016-09-04 01:59:27 +0530

    Added adapetrs to userProfileFrag

--- app/src/main/java/com/aanyajindal/pool_in/UserProfileFragment.java
+++ app/src/main/java/com/aanyajindal/pool_in/UserProfileFragment.java
@@ -3,20 +3,35 @@ package com.aanyajindal.pool_in;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.aanyajindal.pool_in.R;
 import com.aanyajindal.pool_in.models.User;
 
+import java.util.ArrayList;
+
 public class UserProfileFragment extends Fragment {
 
 
     public UserProfileFragment() {
         // Required empty public constructor
+    }
+
+    public static UserProfileFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        UserProfileFragment fragment = new UserProfileFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 
 
@@ -40,6 +55,17 @@ public class UserProfileFragment extends Fragment {
         userEmail.setText(obj.getEmail());
         userCollege.setText(obj.getCollege());
         userLocation.setText(obj.getLocation());
+
+        ArrayList<String> usSkillList = new ArrayList<>();
+
+        ArrayAdapter<String> usSkillAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.list_item_category, usSkillList);
+        userSkills.setAdapter(usSkillAdapter);
+
+        ArrayList<String> usItemlList = new ArrayList<>();
+        
+        ArrayAdapter<String> usItemAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.list_item_category, usItemlList);
+        userItems.setAdapter(usItemAdapter);
+
         return rootView;
     }
 
