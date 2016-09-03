e31db54ab2797669373ed2d69e94a82c3c56abbf
aanyajindal <aanyajindal05@gmail.com>
2016-09-03 21:00:39 +0530

    Added PeopleFragment

---
+++ app/src/main/java/com/aanyajindal/pool_in/PeopleByCategoryFragment.java
@@ -0,0 +1,38 @@
+package com.aanyajindal.pool_in;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PeopleByCategoryFragment extends Fragment {
+
+
+    public PeopleByCategoryFragment() {
+        // Required empty public constructor
+    }
+
+    public static PeopleByCategoryFragment newInstance(String cat) {
+
+        Bundle args = new Bundle();
+        args.putString("catKey", cat);
+        PeopleByCategoryFragment fragment = new PeopleByCategoryFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_people_by_category, container, false);
+    }
+
+}
