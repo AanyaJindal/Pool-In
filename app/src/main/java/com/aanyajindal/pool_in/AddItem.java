c08ec45dfa33d9207af77457cc0481758f096f54
aanyajindal <aanyajindal05@gmail.com>
2016-09-03 22:45:14 +0530

    addech checklist for adding skillset

--- app/src/main/java/com/aanyajindal/pool_in/AddItem.java
+++ app/src/main/java/com/aanyajindal/pool_in/AddItem.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
