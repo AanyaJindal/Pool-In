0711a3269067008cf7e79d2b24daa8c74934d8c9
aanyajindal <aanyajindal05@gmail.com>
2016-09-04 10:36:35 +0530

    Merge branch 'master' of https://github.com/AanyaJindal/Pool-In
    # Conflicts:
    #	app/src/main/res/layout/fragment_user_profile.xml

--- app/src/main/java/com/aanyajindal/pool_in/ItemFragment.java
+++ app/src/main/java/com/aanyajindal/pool_in/ItemFragment.java
@@ -11,6 +11,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.aanyajindal.pool_in.models.Item;
+import com.aanyajindal.pool_in.models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -18,6 +24,7 @@ import java.util.Date;
 
 
 public class ItemFragment extends Fragment {
+    User user;
 
 
     public ItemFragment() {
@@ -25,15 +32,15 @@ public class ItemFragment extends Fragment {
     }
 
     public static ItemFragment newInstance(Item item) {
-        
+
         Bundle args = new Bundle();
-        args.putString("name",item.getName());
-        args.putString("date",item.getDate());
-        args.putString("desc",item.getDesc());
-        args.putString("tags",item.getTags());
-        args.putString("user",item.getUser());
-        args.putString("mode",item.getMode());
-        args.putString("cat",item.getCat());
+        args.putString("name", item.getName());
+        args.putString("date", item.getDate());
+        args.putString("desc", item.getDesc());
+        args.putString("tags", item.getTags());
+        args.putString("user", item.getUser());
+        args.putString("mode", item.getMode());
+        args.putString("cat", item.getCat());
         ItemFragment fragment = new ItemFragment();
         fragment.setArguments(args);
         return fragment;
@@ -42,22 +49,37 @@ public class ItemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.fragment_item, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_item, container, false);
         Bundle bundle = getArguments();
-        final Item obj = new Item(bundle.getString("name"),bundle.getString("user"),bundle.getString("desc")
-        ,bundle.getString("mode"),bundle.getString("cat"),bundle.getString("tags"),bundle.getString("date"));
+        final Item obj = new Item(bundle.getString("name"), bundle.getString("user"), bundle.getString("desc")
+                , bundle.getString("mode"), bundle.getString("cat"), bundle.getString("tags"), bundle.getString("date"));
 
-        TextView itemName = (TextView)rootView.findViewById(R.id.item_name_value);
-        final TextView itemUser = (TextView)rootView.findViewById(R.id.item_user_value);
-        final TextView itemDesc = (TextView)rootView.findViewById(R.id.item_desc_value);
-        TextView itemMode = (TextView)rootView.findViewById(R.id.item_mode_value);
-        TextView itemCategory = (TextView)rootView.findViewById(R.id.item_category_value);
+        TextView itemName = (TextView) rootView.findViewById(R.id.item_name_value);
+        final TextView itemUser = (TextView) rootView.findViewById(R.id.item_user_value);
+        final TextView itemDesc = (TextView) rootView.findViewById(R.id.item_desc_value);
+        TextView itemMode = (TextView) rootView.findViewById(R.id.item_mode_value);
+        TextView itemCategory = (TextView) rootView.findViewById(R.id.item_category_value);
         TextView itemTags = (TextView) rootView.findViewById(R.id.item_tags_value);
         TextView itemDate = (TextView) rootView.findViewById(R.id.item_date_value);
 
 
+        DatabaseReference temp = FirebaseDatabase.getInstance().getReference().child("users").child(obj.getUser());
+        temp.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                user = dataSnapshot.getValue(User.class);
+                itemUser.setText(user.getName());
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         itemName.setText(obj.getName());
-        itemUser.setText(obj.getUser());
         itemDesc.setText(obj.getDesc());
         itemMode.setText(obj.getMode());
         itemCategory.setText(obj.getCat());
@@ -74,6 +96,7 @@ public class ItemFragment extends Fragment {
         }
 
         itemDate.setText(frDate);
+
 
         itemUser.setOnClickListener(new View.OnClickListener() {
             @Override
