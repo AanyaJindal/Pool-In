07506719ea67a57a9d72f19966cb623127412604
jatin-jt <sachinjatingupta@gmail.com>
2017-06-28 21:36:52 +0530

    fixed iamge not showing in my items

--- app/src/main/java/com/aanyajindal/pool_in/AddItem.java
+++ app/src/main/java/com/aanyajindal/pool_in/AddItem.java
@@ -205,7 +205,7 @@ public class AddItem extends AppCompatActivity {
                 itemsList = mainDatabase.child("items");
                 String itemid = itemsList.push().getKey();
                 itemsList.child(itemid).setValue(item);
-                storageRef = FirebaseStorage.getInstance().getReference().child(itemid);
+                storageRef = FirebaseStorage.getInstance().getReference().child("item"+itemid);
                 storageRef.putFile(Uri.parse(mCurrentPhotoPath)).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
